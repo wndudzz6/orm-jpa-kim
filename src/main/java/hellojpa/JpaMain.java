3a0eb5867e62a8ce17d662755c94a9d567bd2344
O ju yeong <osmzxcvb@naver.com>
2025-03-19 19:22:52 +0900

    섹션5.필드와 컬럼 매핑까지 예제

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,61 +12,16 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
-
+        tx.begin();
         try{
-            tx.begin();
-            Member member1 = new Member(13L, "aaaa");
-            em.persist(member1);
-            System.out.println("em.persist(member1 13L aaaa)");
-            System.out.println("==아직 DB에 쿼리x==");
 
-            Member findMember = em.find(Member.class, 13L);
-            System.out.println("find로 13L찾음, JPA는 영속성 컨텍스트에서 엔티티를 찾음 DB x");
-            System.out.println(findMember + "DB가 아닌 영속성 컨텍스트에서 찾은 13L");
+            Member member = new Member();
+            member.setId(3L);
+            member.setName("C");
+            member.setRoleType(RoleType.GUEST);
 
-            member1.setName("bbbb");
-            tx.commit(); //DB에 반영
-            System.out.println("aaaa->bbbb(영속성 컨텍스트) / DB엔 처음부터 bbbb로 저장"+member1);
+            em.persist(member);
 
-
-            System.out.println("====준영속 예제===");
-            tx.begin();
-            Member member2 = new Member(17L, "cccc");
-            em.persist(member2);
-            em.flush(); // DB에 바로 반영
-            System.out.println("==(flush)17L cccc DB에 저장: "+member2+"==");
-
-            em.detach(member2); // member 엔티티를 준영속 상태로 전환
-            member2.setName("dddd"); // 준영속 상태에서 변경해도 DB에 반영되지 않음
-            tx.commit();
-            System.out.println("준영속 상태 dddd로 변경"+member2);
-
-            //삭제 작업 예제
-            System.out.println("==삭제작업=="); //remove는 DB에 있는 데이터까지 삭제. clear는 영속성 컨텍스트와 모든 데이터 분리
-            tx.begin();
-            Member memberToClear = new Member(18L, "clear");
-            System.out.println("18L clear 등록");
-            em.persist(memberToClear);
-            em.clear(); // 영속성 컨텍스트 내 모든 엔티티가 제거됨 - 모든 엔티티가 준영속 상태
-            System.out.println("영속성 컨텍스트 내 18L clear의 모든 엔티티가 제거됨");
-            tx.commit(); // DB에서도 해당 엔티티가 삭제됨
-            List<Member> clearMembers = em.createQuery("select m from Member m", Member.class).getResultList();
-            for (Member clearMember : clearMembers) {
-                System.out.println("Member = " + clearMember);
-            }
-            System.out.println("18L clear 없음");
-
-            System.out.println("JPQL 쿼리 사용==");
-            //JPQL 사용 쿼리 실행 시 JPA는 자동으로 플러시를 호출해서 DB에 반영한다.
-            Member flushMember = new Member(19L, "eeee");
-            em.persist(flushMember);
-            System.out.println("permist(19L eeee)함");
-            // 아래 JPQL 실행 전 자동 플러시 발생
-            List<Member> members = em.createQuery("SELECT m FROM Member m", Member.class).getResultList();
-            System.out.println("JPQL 쿼리로 flush가 호출되어 DB에 저장됨");
-            for (Member member : members) {
-                System.out.println(member);;
-            }
             tx.commit();
 
         }catch (Exception e){
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,21 +1,51 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jdk.jfr.Name;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity//JPA를 사용하는 애구나~
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING) //EnumType.STRING 필수.
+    // default = ORDINAL인데 ENUM 수정하다가 순서 바뀌어도 DB에 적용안됨 - 버그 발생
+    private RoleType roleType;
+
+    //날짜 타입 DATE TIME TIMESTAMP 이렇게 세 가지
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    //최신 하이버네이트는 이거 써라
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    //큰 컨텐츠?
+    @Lob
+    private String description;
+
+    @Transient //DB와 관련없이 메모리에서만 쓰고싶다 할 때 붙임
+    private int temp;
 
     public Member() {
     }
 
     public Member(Long id, String name) {
         this.id = id;
-        this.name = name;
+        this.username = name;
     }
 
     public Long getId() {
@@ -23,22 +53,50 @@ public class Member {
     }
 
     public String getName() {
-        return name;
+        return username;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.username = name;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     @Override
     public String toString() {
         return "Member{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
+                ", name='" + username + '\'' +
                 '}';
     }
 }
